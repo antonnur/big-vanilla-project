4ff922378f8a47d508f8d3906646b48d2f1380b3
Tont <yamalauto1@yandex.ru>
2021-07-27 21:33:35 +0500

    PC Home JS/TS 08_lesson (End)

--- src/08-associative_array/08_01.test.ts
+++ src/08-associative_array/08_01.test.ts
@@ -1,12 +1,26 @@
+type UsersType = {
+  [key: string]: { id: number, name: string }
+}
 
+let userTwo: UsersType
 
-beforeEach(()=>{
-
+beforeEach(() => {
+  userTwo = {
+    '12': {id: 12, name: 'Dmitri'},
+    '32': {id: 32, name: 'Natasha'},
+    '43': {id: 43, name: 'Valera'},
+    '4': {id: 4, name: 'Katya'}
+  }
 })
 
-test('Should select corresponding user from obj', ()=>{
-  expect(usersObj[0]).toBe('Dmitri')
-  expect(usersObj[1]).toBe('Natasha')
-  expect(usersObj[2]).toBe('Valera')
-  expect(usersObj[3]).toBe('Katya')
+test('Should update corresponding user', () => {
+  userTwo['4'].name = 'Dmitri'
+  expect(userTwo['4'].name).toBe('Dmitri')
+  expect(userTwo['4']).toEqual({id: 4, name: 'Dmitri'})
+})
+
+test('Should delete corresponding user', () => {
+  delete userTwo['4']
+
+  expect(userTwo['4']).toBeUndefined()
 })
--- src/08-associative_array/08_01.ts
+++ src/08-associative_array/08_01.ts
@@ -1,3 +1,13 @@
+let userOne = {
+  name: 'Dmitri',
+  age: 12,
+  address: {
+    city: {
+      title: 'Msk'
+    }
+  }
+}
+
 export const userObj = {
   '0': 'Dmitri',
   '1': 'Natasha',
@@ -5,16 +15,32 @@ export const userObj = {
   '3': 'Katya'
 }
 
-export const userArray = [
+type UsersType ={
+  [key:string]: {id: number, name: string}
+}
+
+export let userTwo: UsersType = {
+  '12': {id: 12, name: 'Dmitri'},
+  '32': {id: 32, name: 'Natasha'},
+  '43': {id: 43, name: 'Valera'},
+  '4': {id: 4, name: 'Katya'}
+}
+
+// userTwo[1]
+let user = {id: 100, name: 'Igor'}
+userTwo[user.id] = user
+delete userTwo[user.id]
+userTwo[user.id].name = 'Victor'
+
+export const usersArray = [
   {id: 12, name: 'Dmitri'},
   {id: 32, name: 'Natasha'},
   {id: 43, name: 'Valera'},
   {id: 4, name: 'Katya'}
 ]
 
-export const users = {
-  '12': {id: 12, name: 'Dmitri'},
-  '32': {id: 32, name: 'Natasha'},
-  '43': {id: 43, name: 'Valera'},
-  '4': {id: 4, name: 'Katya'}
-}
+// usersArray.findIndex()
+// usersArray.find(u => u.id ===1)
+// usersArray.push(user)
+// let userCopy = [...userArray.filter(), user]
+// let usersArray = usersArray.filter(u => u.id !== user.id)
